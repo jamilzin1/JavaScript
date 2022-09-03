3ea4e8f150ca82705761e88d2c1fae81f5a52587
jamilzin1 <jamilnetobr@gmail.com>
2022-09-03 16:34:05 -0300

    push

--- String manipulate/test.js
+++ String manipulate/test.js
@@ -1,2 +1,29 @@
-console.log("ok");
-                                 D
+let string1 = "abcdef w";
+let string2 = "cacbdffe";
+
+
+function verify(){
+    let returnString="";
+    let count=0;
+for(let i=0; i<string1.length; i++)
+{
+    for(let j=0; j<string2.length; j++)
+{
+    if(string1.charAt(i)==string2.charAt(j)){
+        if(returnString.includes(string1.charAt(i))==false)
+        {
+        returnString+=string1.charAt(i);
+        count++;
+        }
+    }
+    else
+    {
+    string2.replace(string2.charAt(j), "");
+    }
+
+}
+}
+console.log("Existem "+count+" letras iguais: ");
+return returnString;
+}
+console.log(verify());
